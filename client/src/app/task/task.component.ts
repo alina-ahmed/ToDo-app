02b11d518fffbb9a32a1e34c8e5a1897f85c034a
Alina Ahmed Qureshi <71174202+alina-ahmed@users.noreply.github.com>
2024-11-28 15:09:38 +0500

    Update task.component.ts
    Cleaned code

--- client/src/app/task/task.component.ts
+++ client/src/app/task/task.component.ts
@@ -21,22 +21,6 @@ export class TaskComponent implements OnInit {
 
   constructor(private taskService:TaskService, private dialogModalService:DialogService){}
 
-//   newTaskDesc = '';
-//   finishedTasks: Task[]=[];
-//  taskToBeDeleted: Task | undefined;
-
-//    tasks: Task[] = [];
-
-//    someTask!:{
-//     taskName:string,
-//     taskDescription: string,
-//     taskStatus:string,
-//     taskIcon: string,
-//     taskStatusIcon: [
-//         string,string]
-// };
-
-
   ngOnInit(): void {
     this.fetchTasks();
   }
@@ -54,23 +38,6 @@ export class TaskComponent implements OnInit {
 
   }
 
-  // readonly dialog = inject(MatDialog);
-
-  // openDialog() {
-
-  // this.dialogModalService.openDialog().subscribe(data=>{
-  
-  //   // this.addTasks(data);
-
-  // });
-
-
-  // }
-
-  // openEditDialog(task: Task){
-  //   this.dialogModalService.openEditDialog(task);
-
-  // }
 
   setTaskBgColor(task: Task){
 
@@ -87,45 +54,3 @@ export class TaskComponent implements OnInit {
 
   }
 
-
-
-
-  // getTasks():void{
-  //   this.tasks=this.taskService.getTasks();
-  // }
-
-  // addTasks(data: any):void{
-  
-  //   let newTask : Task;
-
-  //    newTask = {
-  //     "title": data.value.taskName,
-  //     "description": data.value.taskDescription,
-  //     "status":data.value.taskStatus,
-  //     "icon": data.value.taskIcon,
-  //     "statusIcon": data.value.taskStatusIcon
-  //   };
-
-
-  //     // this.taskService.addTask(newTask);
-
-  
-  // }
-
-
-  // deleteTask(task:Task){
-  //   const index: number = this.tasks.indexOf(task);
-  //   let completedTask: Task[];
-  //   if (index !== -1) {
-  //     completedTask=this.tasks.filter(element => element == task);
-  //     this.tasks.splice(index, 1);
-
-  //     console.log(completedTask);
-  //     console.log(completedTask[0]);
-  //     this.finishedTasks.push(completedTask[0]);
-  //     console.log(this.finishedTasks);
-        
-  //   }  
-  // }
-
-}
